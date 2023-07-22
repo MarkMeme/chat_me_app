e0135e6bd4fd37901fb16e16042712e0cf2bdbad
MarkMeme <markmeme04@gmail.com>
2023-07-22 14:15:53 +0300

    initialization firebase and signup and sugnin

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -11,6 +14,33 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   var _isLogin = true;
+  final _formKey = GlobalKey<FormState>();
+  var _enteredEmail = '';
+  var _enteredPass = '';
+
+  void _sebmit() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      try {
+        if (_isLogin) {
+          final authCred = await _firebase.signInWithEmailAndPassword(
+              email: _enteredEmail, password: _enteredPass);
+        } else {
+          final authCred = await _firebase.createUserWithEmailAndPassword(
+              email: _enteredEmail, password: _enteredPass);
+          print(authCred);
+        }
+      } on FirebaseAuthException catch (error) {
+        if (error.code == 'email-already-in-use') {
+          //
+        }
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(error.message ?? 'something went wrong')));
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +67,7 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Padding(
                     padding: const EdgeInsets.all(16),
                     child: Form(
+                      key: _formKey,
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
@@ -46,15 +77,37 @@ class _AuthScreenState extends State<AuthScreen> {
                             keyboardType: TextInputType.emailAddress,
                             autocorrect: false,
                             textCapitalization: TextCapitalization.none,
+                            validator: (value) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  !value.contains('@')) {
+                                return 'Please enter a valid E-mail';
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) {
+                              _enteredEmail = newValue!;
+                            },
                           ),
                           TextFormField(
                             decoration:
                                 const InputDecoration(labelText: 'Password'),
                             obscureText: true,
+                            validator: (value) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  value.length < 6) {
+                                return 'Please enter a valid Password';
+                              }
+                              return null;
+                            },
+                            onSaved: (newValue) {
+                              _enteredPass = newValue!;
+                            },
                           ),
                           const SizedBox(height: 12),
                           ElevatedButton(
-                            onPressed: () {},
+                            onPressed: _sebmit,
                             style: ElevatedButton.styleFrom(
                               backgroundColor: Theme.of(context)
                                   .colorScheme
